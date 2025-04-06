20034cb9f4aa3431c11d71d07c5ca7d161639513
Rundll32 <3161880837@qq.com>
2025-04-06 12:19:42 +0800

    恢复 calc 函数的装饰器，支持多种计算操作；启用相关块模式和类型

--- src/extension.ts
+++ src/extension.ts
@@ -16,24 +16,24 @@ export default class MyExtension extends Extension {
     randomReturn({ data }: { data: string[][] }) {
         return data.flat(Infinity)[Random.integer(0, data.length - 1)];
     }
-    // @BlockMode.Filt("sprite")
-    // @BlockMode.UseMonitor
-    // @BlockMode.ThreadRestartable
-    // @BlockMode.ActiveEdge
-    // @BlockType.Reporter([
-    //     "计算[num1:number=114]+[num2:number=514]",
-    //     "计算[num1:number=114]-[num2:number=514]",
-    //     "计算[num1:number=114]*[num2:number=514]",
-    //     "计算[num1:number=114]/[num2:number=514]"
-    // ]) calc({ num1, num2 }: { num1: number; num2: number }, overloadIndex: number) {
-    //     const overloadMap = [
-    //         (a: number, b: number) => a + b,
-    //         (a: number, b: number) => a - b,
-    //         (a: number, b: number) => a * b,
-    //         (a: number, b: number) => a / b
-    //     ];
-    //     return overloadMap[overloadIndex](num1, num2);
-    // }
+    @BlockMode.Filt("sprite")
+    @BlockMode.UseMonitor
+    @BlockMode.ThreadRestartable
+    @BlockMode.ActiveEdge
+    @BlockType.Reporter([
+        "计算[num1:number=114]+[num2:number=514]",
+        "计算[num1:number=114]-[num2:number=514]",
+        "计算[num1:number=114]*[num2:number=514]",
+        "计算[num1:number=114]/[num2:number=514]"
+    ]) calc({ num1, num2 }: { num1: number; num2: number }, overloadIndex: number) {
+        const overloadMap = [
+            (a: number, b: number) => a + b,
+            (a: number, b: number) => a - b,
+            (a: number, b: number) => a * b,
+            (a: number, b: number) => a / b
+        ];
+        return overloadMap[overloadIndex](num1, num2);
+    }
     @BlockMode.Hidden
     @BlockType.Command("这个积木已隐藏")
     hiddenBlock() { }
